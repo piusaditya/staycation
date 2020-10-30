aba40a3556247df03d39d6e8af9c9d21eb5a28c1
unknown <piusadityanugraha@gmail.com>
2020-10-30 20:58:47 +0700

    done element/form/inputNumber + add inputDate

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,15 @@
+import React, { useState, useRef, useEffect } from "react";
+import propTypes from "prop-types";
+
+import { DateRange } from "react-date-range";
+
+import "./index.scss";
+import "react-date-range/dist/styles.css";
+import "react-date=range/dist/theme/default.css";
+
+import formtDate from "utils/formatDate";
+import iconCalendar from "assets/images/iconCalendar.svg";
+
+export default function index() {
+	return <div></div>;
+}
