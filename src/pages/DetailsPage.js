53b18474559eddc1d3e3512cc88cee28d4d8fec0
Pius Aditya <piusadityanugraha@gmail.com>
2021-04-16 23:17:20 +0700

    ngerapihin indent tab width & i redux

--- src/pages/DetailsPage.js
+++ src/pages/DetailsPage.js
@@ -13,41 +13,41 @@ import Footer from "parts/Footer";
 import ItemDetails from "json/itemDetails.json";
 
 export default class DetailsPage extends Component {
-	componentDidMount() {
-		window.title = "Details Page";
-		window.scrollTo(0, 0);
-	}
+  componentDidMount() {
+    window.title = "Details Page";
+    window.scrollTo(0, 0);
+  }
 
-	render() {
-		const breadcrumb = [
-			{ pageTitle: "Home", pageHref: "" },
-			{ pageTitle: "House Details", pageHref: "" },
-		];
+  render() {
+    const breadcrumb = [
+      { pageTitle: "Home", pageHref: "" },
+      { pageTitle: "House Details", pageHref: "" },
+    ];
 
-		return (
-			<>
-				<Header {...this.props} />
-				<PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
-				<FeaturedImage data={ItemDetails.imageUrls} />
-				<section className="container">
-					<div className="row">
-						<div className="col-7 pr-5">
-							<Fade bottom>
-								<PageDetailDescription data={ItemDetails} />
-							</Fade>
-						</div>
-						<div className="col-5">
-							<Fade bottom>
-								<BookingForm itemDetails={ItemDetails} />
-							</Fade>
-						</div>
-					</div>
-				</section>
+    return (
+      <>
+        <Header {...this.props} />
+        <PageDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
+        <FeaturedImage data={ItemDetails.imageUrls} />
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
+            </div>
+            <div className="col-5">
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
+            </div>
+          </div>
+        </section>
 
-				<Categories data={ItemDetails.categories} />
-				<Testimony data={ItemDetails.testimonial} />
-				<Footer />
-			</>
-		);
-	}
+        <Categories data={ItemDetails.categories} />
+        <Testimony data={ItemDetails.testimonial} />
+        <Footer />
+      </>
+    );
+  }
 }
--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -6,115 +6,115 @@ import Button from "elements/Button";
 import { InputNumber, InputDate } from "elements/Form";
 
 export default class BookingForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			data: {
-				duration: 1,
-				date: {
-					startDate: new Date(),
-					endDate: new Date(),
-					key: "selection",
-				},
-			},
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        duration: 1,
+        date: {
+          startDate: new Date(),
+          endDate: new Date(),
+          key: "selection",
+        },
+      },
+    };
+  }
 
-	updateData = (e) => {
-		this.setState({
-			...this.state,
-			data: {
-				...this.state.data,
-				[e.target.name]: e.target.value,
-			},
-		});
-	};
+  updateData = (e) => {
+    this.setState({
+      ...this.state,
+      data: {
+        ...this.state.data,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
 
-	componentDidUpdate(prevProps, prevState) {
-		//desctructure state es7 redux with shortcut command cs
-		const { data } = this.state;
-		//condition 1 if date in prevState data.date not same with current
-		if (prevState.data.date !== data.date) {
-			const startDate = new Date(data.date.startDate);
-			const endDate = new Date(data.date.endDate);
-			const countDuration = new Date(endDate - startDate).getDate();
-			this.setState({
-				data: {
-					...this.state.data,
-					duration: countDuration,
-				},
-			});
-		}
-		//condition 2 if data.duration in prevState not the same with current
-		if (prevState.data.duration !== data.duration) {
-			const startDate = new Date(data.date.startDate);
-			const endDate = new Date(
-				startDate.setDate(startDate.getDate() + +data.duration - 1)
-			);
-			this.setState({
-				...this.state,
-				data: {
-					...this.state.data,
-					date: {
-						...this.state.data.date,
-						endDate: endDate,
-					},
-				},
-			});
-		}
-	}
+  componentDidUpdate(prevProps, prevState) {
+    //de-structure state es7 redux with shortcut command cs
+    const { data } = this.state;
+    //condition 1 if date in prevState data.date not same with current
+    if (prevState.data.date !== data.date) {
+      const startDate = new Date(data.date.startDate);
+      const endDate = new Date(data.date.endDate);
+      const countDuration = new Date(endDate - startDate).getDate();
+      this.setState({
+        data: {
+          ...this.state.data,
+          duration: countDuration,
+        },
+      });
+    }
+    //condition 2 if data.duration in prevState not the same with current
+    if (prevState.data.duration !== data.duration) {
+      const startDate = new Date(data.date.startDate);
+      const endDate = new Date(
+        startDate.setDate(startDate.getDate() + +data.duration - 1)
+      );
+      this.setState({
+        ...this.state,
+        data: {
+          ...this.state.data,
+          date: {
+            ...this.state.data.date,
+            endDate: endDate,
+          },
+        },
+      });
+    }
+  }
 
-	render() {
-		//destructure state and props
-		const { data } = this.state;
-		const { itemDetails, startBooking } = this.props;
+  render() {
+    //de-structure state and props
+    const { data } = this.state;
+    const { itemDetails, startBooking } = this.props;
 
-		return (
-			<div className="card bordered" style={{ padding: "60px 80px" }}>
-				<h4 className="mb-3">Start Booking</h4>
-				<h5 className="h2 text-teal mb-4">
-					${itemDetails.price}{" "}
-					<span className="text-gray-500 font-weight-light">
-						per {itemDetails.unit}
-					</span>
-				</h5>
-				<label htmlFor="duration">How long you will stay?</label>
-				<InputNumber
-					max={30}
-					suffix={" night"}
-					isSuffixPlural
-					onChange={this.updateData}
-					name="duration"
-					value={data.duration}
-				/>
-				<label htmlFor="date">Pick a date</label>
-				<InputDate onChange={this.updateData} name="date" value={data.date} />
-				<h6 className="text-gray-500 font-weight-light">
-					You will pay{" "}
-					<span className="text-gray-900">
-						${itemDetails.price * data.duration} USD
-					</span>{" "}
-					per{" "}
-					<span className="text-gray-900">
-						{data.duration} {itemDetails.unit}
-					</span>
-				</h6>
+    return (
+      <div className="card bordered" style={{ padding: "60px 80px" }}>
+        <h4 className="mb-3">Start Booking</h4>
+        <h5 className="h2 text-teal mb-4">
+          ${itemDetails.price}{" "}
+          <span className="text-gray-500 font-weight-light">
+            per {itemDetails.unit}
+          </span>
+        </h5>
+        <label htmlFor="duration">How long you will stay?</label>
+        <InputNumber
+          max={30}
+          suffix={" night"}
+          isSuffixPlural
+          onChange={this.updateData}
+          name="duration"
+          value={data.duration}
+        />
+        <label htmlFor="date">Pick a date</label>
+        <InputDate onChange={this.updateData} name="date" value={data.date} />
+        <h6 className="text-gray-500 font-weight-light">
+          You will pay{" "}
+          <span className="text-gray-900">
+            ${itemDetails.price * data.duration} USD
+          </span>{" "}
+          per{" "}
+          <span className="text-gray-900">
+            {data.duration} {itemDetails.unit}
+          </span>
+        </h6>
 
-				<Button
-					className="btn"
-					hasShadow
-					isPrimary
-					isBlock
-					onClick={startBooking}
-				>
-					Continue to Book
-				</Button>
-			</div>
-		);
-	}
+        <Button
+          className="btn"
+          hasShadow
+          isPrimary
+          isBlock
+          onClick={startBooking}
+        >
+          Continue to Book
+        </Button>
+      </div>
+    );
+  }
 }
 
 BookingForm.propTypes = {
-	itemDetails: propTypes.object,
-	startBooking: propTypes.func,
+  itemDetails: propTypes.object,
+  startBooking: propTypes.func,
 };
